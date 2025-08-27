a512b8ff5d5722c6c42c08b0dc5e09263b44be1d
TemmyJewel <temmyjewel22@gmail.com>
2025-08-27 17:29:35 +0100

    another commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const questions = [
     {
         question: "What's the full meaning of HTML",
         answers: [
-            {text: "Hype Text Markup Load", correct: false},
-            {text: "Hyper Test Markup Language", correct: false},
-            {text: "Hyper Text Markup Language", correct: true},
-            {text: "Hyper Text Makeup Language", correct: false},
+            {text: "Hype Text Markup Load", correct: "false"},
+            {text: "Hyper Test Markup Language", correct: "false"},
+            {text: "Hyper Text Markup Language", correct: "true"},
+            {text: "Hyper Text Makeup Language", correct: "false"}
         ]
     },
      {
@@ -47,17 +47,79 @@ let score = 0;
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestions();
 }
 
 function showQuestions(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    questionElement. innerHTML = currentQuestion.question;
-    currentQuestion.answers.forEach(answers => {
-        document.createElement("button");
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+       const button = document.createElement("button");
+       button.innerHTML = answer.text;
+       button.classList.add("btn");
+       answerButton.appendChild(button);
+       if(answer.correct){
+        button.dataset.correct = answer.correct;
+       }
+       button.addEventListener("click", selectanswer)
     })
 }
 
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+
+function selectanswer(e){
+    const selectedbtn = e.target;
+    const isCorrect = selectedbtn.dataset.correct === "true";
+
+    if(isCorrect){
+        selectedbtn.classList.add("correct");
+        score++;
+    }else{
+        selectedbtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `Congratulations! You scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestions();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
 startQuiz();
-showQuestions();
+
+
 
 
